c42e0aaaf115e85337d3727a38223cc8d5ca7b3d
Roger Kowalewski <roger.kowalewski@gmail.com>
2015-08-03 16:44:43 +0200

    modified tests

--- samples/runSamples.sh
+++ samples/runSamples.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
 #export LD_PRELOAD=./lib/libnasty_mpi.so;
-error=0;
+rm -f samples/tests.log
+error=0
+tests=()
 
 for file in $(find bin -type f -executable);
 do
+  tests=("${tests[@]}" "$file");
+done
+
+total=${#tests[@]}
+
+for ((i=0; i < $total; i++))
+do
+  file=${tests[i]};
   echo "-------------Running Test: ${file} ---------------" >> samples/tests.log;
-  mpirun -n 48 ${file}>>samples/tests.log 2>&1;
-  if [ $? -eq 0 ];
+  #echo "-------------Running Test: ${file} ---------------";
+  counter=0;
+  while [ ${error} -eq 0 -a ${counter} -lt 10 ];
+  do
+    mpirun -n 48 ${file}>>samples/tests.log 2>&1;
+    if [ $? -ne 0 ];
+    then
+      error=1;
+      echo $file ERROR;
+    fi
+    counter=$((counter + 1));
+  done
+
+  if [ ${error} -eq 0 ]
   then
     echo $file PASS;
-  else
-    error=1;
-    echo $file ERROR;
+  fi
+
+  if [ $i -ne $((total - 1)) ]
+  then
+    error=0;
   fi
 done
 
