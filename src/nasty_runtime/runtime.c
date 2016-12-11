4c2d3a908762a5150a4f8ee547e8d01281a85f1d
Roger Kowalewski <kowalewski@nm.ifi.lmu.de>
2016-12-11 21:22:16 +0100

    Fixed compiler error

--- src/nasty_runtime/runtime.c
+++ src/nasty_runtime/runtime.c
@@ -338,7 +338,7 @@ int nasty_mpi_execute_cached_calls(MPI_Win win, int target_rank, bool mayFlush)
 
     if (DArray_is_empty(ops))
     {
-      DArray_clear_estroy(ops);
+      DArray_clear_destroy(ops);
       continue;
     }
 
